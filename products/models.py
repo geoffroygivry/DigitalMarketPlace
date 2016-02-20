b5e44031bf902f9fc0c7445549b6020176fddf7f
Geoffroy Givry <geoff.givry@gmail.com>
2016-02-20 09:39:23 -0500

    added slug and pre_save method

--- products/models.py
+++ products/models.py
@@ -1,4 +1,6 @@
 from __future__ import unicode_literals
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
 
 from django.db import models
 
@@ -6,9 +8,17 @@ from django.db import models
 
 class Product(models.Model):
   title = models.CharField(max_length=30) # title string creation
+  slug = models.SlugField(blank=True)
   description = models.TextField()
   price = models.DecimalField(max_digits=100, decimal_places=2, default=9.99, null=True, blank=True)
   sale_price = models.DecimalField(max_digits=100, decimal_places=2, default= 6.99, null=True, blank=True)
   
   def __str__(self):
     return self.title # returns the title name of the product when you print the object
+  
+def product_pre_save_receiver(sender, instance, *args, **kwargs):
+  if not instance.slug:
+    instance.slug = slugify(instance.title)
+    
+    
+pre_save.connect(product_pre_save_receiver, sender=Product)
--- products/views.py
+++ products/views.py
@@ -1,13 +1,36 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 # Create your views here.
+
+
+def detail_slug_view(request, slug=None):
+  # view of 1 item
+  if request.user.is_authenticated():
+      product = get_object_or_404(Product, slug=slug)
+#         try:
+#           product = Product.objects.get(id=object_id)
+#         except Product.DoesNotExist:
+#           product = None
+      template = 'detail_view.html'
+      context = {
+        "product" : product
+      }
+  else:
+    template = 'not_found.html'
+    context = {}
+  return render(request, template, context)
 
 
 def detail_view(request, object_id=None):
   # view of 1 item
   if object_id is not None:
     if request.user.is_authenticated():
-        product = Product.objects.get(id=object_id)
+        product = get_object_or_404(Product, id=object_id)
+#         try:
+#           product = Product.objects.get(id=object_id)
+#         except Product.DoesNotExist:
+#           product = None
         template = 'detail_view.html'
         context = {
           "product" : product
